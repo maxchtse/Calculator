56ab780bed0e71276691baf79c2c6d8abe2d284e
maxchtse <tsechunghei97@outlook.com>
2024-01-13 21:53:17 +0800

    Add result block

--- app/src/main/java/com/company/calculator/MainActivity.java
+++ app/src/main/java/com/company/calculator/MainActivity.java
@@ -3,12 +3,27 @@ package com.company.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView resultTv, solutionTv;
+    MaterialButton buttonC, buttonBrackOpen, getButtonBrackClose;
+    MaterialButton buttonDivide, buttonMultiply, buttonPlus, buttonMinus, buttonEquals;
+    MaterialButton button0, button1, button2, button3, button4, button5, button6, button7, button8, button9;
+    MaterialButton buttonAC, buttonDot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
