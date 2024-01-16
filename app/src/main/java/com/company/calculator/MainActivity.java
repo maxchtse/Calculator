4b40fbf60f1c3fe59fdb08d5f207dd3c30ea9b5e
maxchtse <tsechunghei97@outlook.com>
2024-01-16 16:20:39 +0800

    Add functionality of the buttons

--- app/src/main/java/com/company/calculator/MainActivity.java
+++ app/src/main/java/com/company/calculator/MainActivity.java
@@ -11,7 +11,7 @@ import com.google.android.material.button.MaterialButton;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView resultTv, solutionTv;
-    MaterialButton buttonC, buttonBrackOpen, getButtonBrackClose;
+    MaterialButton buttonC, buttonBrackOpen, ButtonBrackClose;
     MaterialButton buttonDivide, buttonMultiply, buttonPlus, buttonMinus, buttonEquals;
     MaterialButton button0, button1, button2, button3, button4, button5, button6, button7, button8, button9;
     MaterialButton buttonAC, buttonDot;
@@ -20,10 +20,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        resultTv = findViewById(R.id.result_tv);
+        solutionTv = findViewById(R.id.solution_tv);
+
+        assignID(buttonC, R.id.button_c);
+        assignID(buttonBrackOpen, R.id.button_open_bracket);
+        assignID(ButtonBrackClose, R.id.button_close_bracket);
+        assignID(buttonDivide, R.id.button_divide);
+        assignID(buttonMultiply, R.id.button_mulitply);
+        assignID(buttonPlus, R.id.button_plus);
+        assignID(buttonMinus, R.id.button_minus);
+        assignID(buttonEquals, R.id.button_equals);
+        assignID(button0, R.id.button_0);
+        assignID(button1, R.id.button_1);
+        assignID(button2, R.id.button_2);
+        assignID(button3, R.id.button_3);
+        assignID(button4, R.id.button_4);
+        assignID(button5, R.id.button_5);
+        assignID(button6, R.id.button_6);
+        assignID(button7, R.id.button_7);
+        assignID(button8, R.id.button_8);
+        assignID(button9, R.id.button_9);
+        assignID(buttonAC, R.id.button_0);
+        assignID(buttonDot, R.id.button_dot);
+    }
+
+    void assignID(MaterialButton btn, int id) {
+        btn = findViewById(id);
+        btn.setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View v) {
-
+    public void onClick(View view) {
+        MaterialButton button = (MaterialButton) view;
+        String buttonText = button.getText().toString();
+        solutionTv.setText(buttonText);
     }
 }
